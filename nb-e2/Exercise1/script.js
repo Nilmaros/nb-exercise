9d1654fa3c8101b05f96d9e6d1758ed9a23770a4
Mit <mit.broadhurst@gmail.com>
2019-07-11 09:25:45 +0100

    with exercise 1 included

---
+++ nb-e2/Exercise1/script.js
@@ -0,0 +1,7 @@
+﻿function replaceLetterWithPosition(str) {
+    var alphabet = "abcdefghijklmnopqrstuvwxyz";
+    document.getElementById("result").innerHTML = "";
+    [...str.replace(/[^a-z]+/gi, '')].forEach(letter => {
+        document.getElementById("result").innerHTML += alphabet.indexOf(letter.toLowerCase()) + 1 + " ";
+    })
+};
